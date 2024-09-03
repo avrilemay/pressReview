12d9d894dc46cc438db4dd4fb9e3fbf300d8c4b7
Avrile <112031027+avrilemay@users.noreply.github.com>
2024-09-03 09:38:41 -0500

    app.py

--- app.py
+++ app.py
@@ -151,8 +151,8 @@ if st.button("Générer la revue de presse"):     # bouton pour générer la rev
                         sortie += (f"\n\n\n⠨⠞⠊⠞⠗⠑⠒ {traduction(art['title'])}\n\n⠨⠎⠕⠥⠗⠉⠑⠒ "
                                    f"   {traduction(art['source']['name'])}\n⠨⠏⠥⠃⠇⠊⠿ ⠇⠑⠒ "
                                    f"   {traduction(art['publishedAt'])}\n⠨⠥⠗⠇⠒ "
-                                   f"   {traduction(art['url'])}\n"
-                                   f"   {traduction(contenu)}\n"
+                                   f"   {traduction(art['url'])}\n\n"
+                                   f"{traduction(contenu)}\n"
                                    f"⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶⠶")
                 except Exception as e:
                     st.error(f"Erreur lors de la récupération de l'article : {e}")
