7272380e05ab7cca5b54e0a4a62402071bc58f93
Avrile <112031027+avrilemay@users.noreply.github.com>
2024-09-03 10:58:02 -0500

    app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ if st.button("Générer la revue de presse"):     # bouton pour générer la rev
                                    f"{contenu}\n\n"
                                    f"------------------------------\n\n")
                     else:   # si c'est en braille
-                        contenu = re.sub(r'\n{3,}', '\n\n', contenu)
+                        contenu = re.sub(r'\n{2,}', '\n', contenu)
                         sortie += (
                             f"\n\n\n⠨⠞⠊⠞⠗⠑⠒ {traduction(art['title'])}\n\n"  # Ajoute le titre
                             f"⠨⠎⠕⠥⠗⠉⠑⠒ {traduction(art['source']['name'])}\n\n"  # Ajoute la source et un saut de ligne propre
