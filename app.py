eeeb4ec7e7c31bf7c68434b497aac0e86a5b0dbb
Avrile <112031027+avrilemay@users.noreply.github.com>
2025-03-05 13:43:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ if st.button("Générer la revue de presse"):  # bouton pour générer la revue 
 
                 # buffer pour stocker le fichier PDF
                 buffer = BytesIO()
-                pdf_data = pdf.output(dest='S').encode('latin1')  # 'S' : contenu sous forme de str
+                pdf_data = pdf.output(dest='S').encode('latin1', errors='ignore')
                 buffer.write(pdf_data)
                 buffer.seek(0)  # retour au début du buffer
 
